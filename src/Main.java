e4e64f9bb68ee6dd2c015af3c797c1e84d9ee46d
DrVa97 <vdrumea@griddynamics.com>
2023-02-07 14:22:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import automaton.FiniteAutomaton;
+import grammar.Grammar;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Grammar grammar = new Grammar()
+        {
+            @Override
+            public String generateWord()
+            {
+                return null;
+            }
+
+            @Override
+            public FiniteAutomaton toFiniteAutomaton()
+            {
+                return null;
+            }
+        };
+
+        System.out.println(grammar.generateWord());
+        grammar.generateWord();
+        grammar.generateWord();
+        grammar.generateWord();
+        grammar.generateWord();
+
+
+    }
+}
